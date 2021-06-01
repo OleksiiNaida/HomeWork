516d2ab9aba5ca67b530967fd560c5e8290d6e66
OleksiiNaida <alyoshkin123@gmail.com>
2021-06-01 09:39:21 +0300

    fixed hw19

--- Home Work19 - События/js/script.js
+++ Home Work19 - События/js/script.js
@@ -8,42 +8,51 @@ btnAdd.addEventListener('click', taskAdd);
 
 function taskAdd(event){
    event.preventDefault();
-   // if(addTask.value == 0) btnAdd.setAttribute('disabled', 'disabled');
-   let inputTask = document.createElement('li');
-   inputTask.classList.add('task__item');
-   let goTodoList = document.createElement('input');
-   goTodoList.setAttribute('class', 'checkbox');
-   goTodoList.setAttribute('type', 'checkbox');
-   inputTask.appendChild(goTodoList);
-   let tastText = document.createElement('span');
-   tastText.innerText = addTask.value;
-   taskList.appendChild(inputTask);
-   inputTask.appendChild(tastText);
-   addTask.value = '';
+   if(addTask.value !== ""){
+      let inputTask = document.createElement('li');
+      inputTask.classList.add('task__item');
+      let goTodoList = document.createElement('input');
+      goTodoList.setAttribute('class', 'checkbox');
+      goTodoList.setAttribute('type', 'checkbox');
+      goTodoList.addEventListener('change', doneToDo);
+      inputTask.appendChild(goTodoList);
+      let tastText = document.createElement('span');
+      tastText.innerText = addTask.value;
+      inputTask.appendChild(tastText);
+      taskList.appendChild(inputTask);
+      addTask.value = '';
+   }
 }
 
 //Checkbox
 
-let checkedTask = document.querySelectorAll('[type="checkbox"]');
+let checkedTask = document.querySelectorAll('.task__item [type="checkbox"]');
 
 for(let i = 0; i < checkedTask.length; i++){
-   checkedTask[i].addEventListener('change', function(){
-      let ItemComplete = document.createElement('li');
-      ItemComplete.classList.add('completed__item');
-      let goToCompleted = document.createElement('a');
-      goToCompleted.setAttribute('class', 'delete');
-      checkedTask[i].style.display = 'none';
-      goToCompleted.textContent = 'delete';
-      ItemComplete.appendChild(goToCompleted);
-      ItemComplete.insertBefore(checkedTask[i].nextSibling, goToCompleted);
-      completedList.appendChild(ItemComplete);
-   });
-
-   ////delete 
-   let deleteTask = document.querySelectorAll('.delete');
-   let parent = document.querySelectorAll('.completed__item');
-   deleteTask[i].addEventListener('click', function(){
-      deleteTask[i].parentNode.remove();
-   });
+   checkedTask[i].addEventListener('change', doneToDo);
 }
 
+function doneToDo(){
+   let nameToDo = this.nextElementSibling.innerText;
+   taskList.removeChild(this.parentElement);
+   let ItemComplete = document.createElement('li');
+   ItemComplete.classList.add('completed__item');
+   ItemComplete.innerHTML = `<span>${nameToDo}</span>`
+   let goToCompleted = document.createElement('a');
+   goToCompleted.setAttribute('class', 'delete');
+   goToCompleted.textContent = 'delete';
+   goToCompleted.addEventListener('click', removeToDo)
+   ItemComplete.appendChild(goToCompleted);
+   completedList.appendChild(ItemComplete);
+}
+
+//delete
+let deleteTask = document.querySelectorAll('.delete');
+
+for(let i = 0; i < deleteTask.length; i++){
+	deleteTask[i].addEventListener('click', removeToDo)
+}
+
+function removeToDo(){
+   completedList.removeChild(this.parentElement);
+}
